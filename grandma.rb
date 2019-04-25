0cfdcdbffbf7b5235ba7ca6653f92d7a70a65b2f
Rina Kawamura <rina.kawamura@yale.edu>
2019-04-25 17:41:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(str)
   if str == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif str == str.uppercase
+  elsif str == str.upcase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
